76c42516a53ee558c74de73aecca6252d665a7b0
Mertcali <calisahmetmert@gmail.com>
2022-12-01 17:05:54 +0300

    api_update_2

--- ecommerce-demo-4/ecommerce-demo-4/src/main/java/com/etiya/ecommercedemo4/api/controllers/OrderDetailsController.java
+++ ecommerce-demo-4/ecommerce-demo-4/src/main/java/com/etiya/ecommercedemo4/api/controllers/OrderDetailsController.java
@@ -4,6 +4,7 @@ import com.etiya.ecommercedemo4.business.abstracts.IOrderDetailService;
 import com.etiya.ecommercedemo4.entities.concretes.OrderDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class OrderDetailsController {
     }
 
     @GetMapping("{id}")
-    public OrderDetail getById(int id){
+    public OrderDetail getById(@PathVariable int id){
         return this.orderDetailService.getById(id);
     }
 
