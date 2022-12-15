24a8f9ae9916856fe7fa680872880e1294646e72
MertCalis <mert.calis@etiya.com>
2022-12-15 10:38:29 +0300

    PAGINATION - SLICE - MULTI LANGUAGE SUPPORT ADDED

--- ecommerce-demo-4/ecommerce-demo-4/src/main/java/com/etiya/ecommercedemo4/business/abstracts/IAddressService.java
+++ ecommerce-demo-4/ecommerce-demo-4/src/main/java/com/etiya/ecommercedemo4/business/abstracts/IAddressService.java
@@ -6,6 +6,9 @@ import com.etiya.ecommercedemo4.business.dtos.response.address.GetAddressDto;
 import com.etiya.ecommercedemo4.core.util.results.DataResult;
 import com.etiya.ecommercedemo4.core.util.results.Result;
 import com.etiya.ecommercedemo4.entities.concretes.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 
@@ -16,6 +19,8 @@ public interface IAddressService {
     DataResult<Address> getById(int id);
     DataResult<List<Address>> getByAddressType();
     DataResult<GetAddressDto> getAddressDto(int id);
+
+    Page<Address> getAllWithPagination(Pageable pageable);
 
     /*Country getCountryByCityId(int id);
     City getCityByTownId(int id);
